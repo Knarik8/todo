17348c8afeec4ebd0fb5535702141d1242ac96cc
Armine8 <armisha15@mail.ru>
2022-04-09 02:11:03 +0300

    lesson-7

--- todo_list/usersapp/admin.py
+++ todo_list/usersapp/admin.py
@@ -1,6 +1,20 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 
 from usersapp.models import CustomUser
 
+#расширенная версия
+class CustomUserAdmin(UserAdmin):
+    add_fieldsets = (
+        (
+            None,
+            {"classes": ("wide",), "fields":
+                (
+                    "username", "password1", "password2", "email")},
+        ),
+    )
 
-admin.site.register(CustomUser)
+admin.site.register(CustomUser, CustomUserAdmin)
+
+
+# можно просто admin.site.register(CustomUser)
